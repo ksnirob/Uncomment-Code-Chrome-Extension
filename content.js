771d6f7fc045b8ce2804f7988698bfde7dfa5f2b
Khaled Saifullah <khaled.saifullah>
2024-02-14 14:52:44 +0600

    mobile image issue has been fixed

--- content.js
+++ content.js
@@ -3,17 +3,19 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
         var htmlContent = document.documentElement.innerHTML;
 
-        // Uncomment the code excluding <style> and <!--[if mso]> <![endif]-->
         var uncommentedCode = htmlContent.replace(/<!--(?!<!\[endif\]-->)\s*([\s\S]*?)\s*-->/g, function(match, group1) {
-            return group1.replace(/<style>[\s\S]*?<\/style>/g, '') // Remove content inside <style> tags
+            var uncommentedCodeFix = group1.replace(/<style>[\s\S]*?<\/style>/g, '');
+            uncommentedCodeFix += '<img>';
+
+            return uncommentedCodeFix;
         });
 
         try {
-            // Set the uncommented code to the body's innerHTML
             document.body.innerHTML = uncommentedCode;
         } catch (error) {
             console.error("Error setting innerHTML:", error);
         }
+
 
     }
      
